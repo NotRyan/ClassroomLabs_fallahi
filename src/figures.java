b8b48e1dadcdbac9a1fc71f5391061d751663a96
Nathan Fallahi <nathanfallahi@gmail.com>
2015-09-14 09:59:36 -0700

    initial commit
    create shapes

---
+++ src/figures.java
@@ -0,0 +1,51 @@
+/*
+ * ClassroomLabs - I guess this figures
+ * Nathan Fallahi
+ * init 2015-09-14
+ * last 2015-09-14
+ */
+public class figures {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// egg
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+		
+		// break
+		System.out.println();
+		
+		// tea cup
+		System.out.println("\\        /");
+		System.out.println(" \\      /");
+		System.out.println("  ------");
+		System.out.println("+--------+");
+		
+		//break
+		System.out.println();
+		
+		// stop sign
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("|  STOP  |");
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+		System.out.println("  ______");
+
+		//break
+		System.out.println();
+		
+		// hat
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("+--------+");
+		
+	}
+
+}
