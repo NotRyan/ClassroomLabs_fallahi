d3b01be8948a56b8a77219c24a7c1060098dfbc6
Nathan Fallahi <nathanfallahi@gmail.com>
2015-09-15 08:37:27 -0700

    added figures2

---
+++ src/figures2.java
@@ -0,0 +1,61 @@
+/*
+ * Figures2 - Apparently this also figures
+ * Nathan Fallahia
+ * init 2015-09-15
+ * last 2015-09-15
+ */
+
+public class figures2 {
+	
+	public static void main(String[] args) {
+		egg();
+		System.out.println();
+		teacup();
+		System.out.println();
+		stopsign();
+		System.out.println();
+		hat();
+	}
+	
+	/*
+	 * Draw egg
+	 */
+	public static void egg() {
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+	}
+	
+	/*
+	 * Draw tea cup
+	 */
+	public static void teacup() {
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+		System.out.println("+--------+");
+	}
+	
+	/*
+	 * Draw stop sign
+	 */
+	public static void stopsign() {
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("|  STOP  |");
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+	}
+	
+	/*
+	 * draw hat
+	 */
+	public static void hat() {
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("+--------+");
+	}
+}
